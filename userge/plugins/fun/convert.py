831a64349d78356dc1ad81287a1d98986ac59ff7
Ashiney <65082584+Ashiney@users.noreply.github.com>
2020-05-11 23:07:07 +0530

    Add files via upload

---
+++ userge/plugins/fun/convert.py
@@ -0,0 +1,56 @@
+# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
+#
+# This file is part of < https://github.com/UsergeTeam/Userge > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
+#
+# All rights reserved.
+
+
+from userge import userge, Message
+
+
+@userge.on_cmd("small", about={
+    'header': "Make caps smaller",
+    'usage': ".small [text | reply to msg]"})
+async def small_(message: Message):
+    text = message.input_str
+    if message.reply_to_message:
+        text = message.reply_to_message.text
+
+    if not text:
+        await message.err("input not found")
+        return
+
+    await message.edit(text.translate(str.maketrans("ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                                                    "ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘqʀꜱᴛᴜᴠᴡxʏᴢ")))
+
+
+@userge.on_cmd("lower", about={
+    'header': "Convert text to lowwer",
+    'usage': ".lower [text | reply to msg]"})
+async def lower_(message: Message):
+    text = message.input_str
+    if message.reply_to_message:
+        text = message.reply_to_message.text
+
+    if not text:
+        await message.err("input not found")
+        return
+
+    await message.edit(text.lower())
+
+
+@userge.on_cmd("upper", about={
+    'header': "Convert text to upper",
+    'usage': ".upper [text | reply to msg]"})
+async def upper_(message: Message):
+    text = message.input_str
+    if message.reply_to_message:
+        text = message.reply_to_message.text
+
+    if not text:
+        await message.err("input not found")
+        return
+
+    await message.edit(text.upper())
